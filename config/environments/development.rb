778cf0e34a5e85e92d5cbf4dcdd35c7189e990e9
Kristjan Rang <mail@rang.ee>
2014-02-11 17:59:15 +0200

    TurboDev!

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,5 +26,8 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  require 'middleware/turbo_dev'
+  config.middleware.insert 0, Middleware::TurboDev
 end
 STDOUT.sync = true
