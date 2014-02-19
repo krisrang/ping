6cc9c6005c096a317f0575f959351e36c2815c19
Kristjan Rang <mail@rang.ee>
2014-02-19 14:36:26 +0200

    Tidy

--- app/assets/javascripts/ping/routes/room_route.js
+++ app/assets/javascripts/ping/routes/room_route.js
@@ -8,15 +8,5 @@ Ping.RoomRoute = Ping.Route.extend({
 
     room.join();
     this.controllerFor('application').set('room', room.get('id'));
-  },
-
-  setupController: function(controller, model) {
-    var content = Em.A([]);
-    for (var i = 0; i < 100; i++) {
-      content.pushObject(Em.Object.create({message: "Item " + i}));
-    }
-
-    model.set('messages', content);
-    controller.set('model', model);
   }
 });
