b3e201973fed5c274a7c759d8ccc31a44245125c
Kristjan Rang <mail@rang.ee>
2014-02-23 23:52:25 +0200

    Remove old attr from UserSerializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ApplicationSerializer
-  attributes :id, :username, :avatar_template, :previous_visit, :last_seen, :days_visited, :online
+  attributes :id, :username, :avatar_template, :previous_visit, :last_seen, :days_visited
 
   def days_visited
     object.user_stat.days_visited
